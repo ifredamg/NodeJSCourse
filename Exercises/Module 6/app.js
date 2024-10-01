11bd7f53f1eac47f50e5ccf86493715d158bab48
ifredamg <fsraen@gmail.com>
2024-10-01 23:16:41 +0100

    Finishing the pug template

--- Exercises/Module 6/app.js
+++ Exercises/Module 6/app.js
@@ -3,7 +3,9 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
 const app = express();
+
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
@@ -18,8 +20,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404');
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
