d22cf4aacc559ec987301c713a3e75df5ffa1c82
ifredamg <fsraen@gmail.com>
2024-10-29 21:21:33 +0000

    111. Storing Product Data

--- Exercises/Module 7/controllers/admin.js
+++ Exercises/Module 7/controllers/admin.js
@@ -1,27 +1,32 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', { 
-      pageTitle: 'Add Product', 
-      path: '/admin/add-product', 
-      formsCSS: true, 
-      productCSS: true, 
-      activeAddProduct: true 
-    });
+  res.render('admin/add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 };
 
 exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
-    product.save();
-    res.redirect('/');
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+
+  const product = new Product(title, imageUrl, description, price);
+  product.save();
+  res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render('admin/product-list', {
-        prods: products,
-        pageTitle: 'Admin Products',
-        path: '/admin/product-list',
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/product-list',
     });
   });
 }
