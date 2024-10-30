2d42b239e3107961212927a8fda52043107d6a27
Frederico Santos <fred.santos@outlook.pt>
2024-10-30 13:42:02 +0000

    124. Adding a Cart Model

--- Exercises/Module 7/routes/admin.js
+++ Exercises/Module 7/routes/admin.js
@@ -7,7 +7,9 @@ const adminController = require('../controllers/admin');
 const router = express.Router();
 
 router.get('/add-product', adminController.getAddProduct);
+
 router.get('/product-list', adminController.getProducts);
+
 router.post('/add-product', adminController.postAddProduct);
 
 exports.routes = router;
