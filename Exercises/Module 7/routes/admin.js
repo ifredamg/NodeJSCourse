cd76c7a2b2be34812bc21553ad60a77675a0978b
Frederico Santos <fred.santos@outlook.pt>
2024-11-04 22:12:50 +0000

    128. Editing the Product Data

--- Exercises/Module 7/routes/admin.js
+++ Exercises/Module 7/routes/admin.js
@@ -12,4 +12,8 @@ router.get('/product-list', adminController.getProducts);
 
 router.post('/add-product', adminController.postAddProduct);
 
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
 exports.routes = router;
