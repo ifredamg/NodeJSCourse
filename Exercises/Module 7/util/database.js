6811e82d0b939631832c052464e6393a80d7944a
Frederico Santos <fred.santos@outlook.pt>
2024-11-28 12:17:23 +0000

    Start module 11 - Undersanting Sequelize | 152. Connecting to the Database

--- Exercises/Module 7/util/database.js
+++ Exercises/Module 7/util/database.js
@@ -1,11 +1,7 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: '37.187.48.120',
-    user: 'ifredapps',
-    database: 'node-complete',
-    password: '_xCODE/24_',
-    port: 3306
+const sequelize = new Sequelize('node-complete', 'ifredapps', '_xCODE/24_', {
+    port: 3306, dialect: 'mysql', host: '37.187.48.120'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
