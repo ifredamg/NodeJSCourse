a52c3eeba9dbcd4dc02778709ebf6aba4febcd2e
Frederico Santos <fred.santos@outlook.pt>
2024-11-28 13:25:41 +0000

    161. Deleting Products

--- Exercises/Module 7/controllers/admin.js
+++ Exercises/Module 7/controllers/admin.js
@@ -21,6 +21,7 @@ exports.postAddProduct = (req, res, next) => {
   })
     .then(result => {
       console.log(result);
+      res.redirect('/admin/product-list');
     })
     .catch(err => {
       console.log(err);
@@ -90,6 +91,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/product-list')
+  Product.findByPk(prodId)
+    .then(product => {
+      return product.destroy();
+    })
+    .then(result => {
+      console.log("DESTROYED PRODUCT");
+      res.redirect('/admin/product-list')
+    })
+    .catch(err => console.log(err));
 }
