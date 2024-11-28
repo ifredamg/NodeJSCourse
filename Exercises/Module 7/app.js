0ab0791b90d91290160bbdfa00cad7788bfe94d0
Frederico Santos <fred.santos@outlook.pt>
2024-11-28 12:31:53 +0000

    154. Syncing JS Definitions to the Database

--- Exercises/Module 7/app.js
+++ Exercises/Module 7/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -21,5 +21,14 @@ app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+sequelize
+    .sync()
+    .then(results => {
+        //console.log(results);
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
 app.listen(3000);
