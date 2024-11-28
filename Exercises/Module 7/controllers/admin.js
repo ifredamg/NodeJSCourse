fb8783d2899d432f0e965e6168ab3eb67a648c88
Frederico Santos <fred.santos@outlook.pt>
2024-11-28 12:51:18 +0000

    155. Inserting Data & Creating a Product

--- Exercises/Module 7/controllers/admin.js
+++ Exercises/Module 7/controllers/admin.js
@@ -13,14 +13,19 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description
+  })
+  .then(result => {
+    console.log(result);
+  })
+  .catch(err => {
+    console.log(err);
+  })
 
-  const product = new Product(null, title, imageUrl, description, price);
-  product
-    .save()
-    .then(() => {
-      res.redirect('/');
-    })
-    .catch(err => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
