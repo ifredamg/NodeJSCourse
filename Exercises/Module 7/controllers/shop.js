f7e1dde7a42b5cf164b02fcf50c403c025db5bb5
Frederico Santos <fred.santos@outlook.pt>
2024-11-30 12:49:20 +0000

    174. Resetting the Cart & Fetching and Outputting Orders

--- Exercises/Module 7/controllers/shop.js
+++ Exercises/Module 7/controllers/shop.js
@@ -109,9 +109,12 @@ exports.postCartDeleteProduct = (req, res, next) => {
 }
 
 exports.postOrder = (req, res, next) => {
+    let fetchedCart;
+
     req.user
         .getCart()
         .then(cart => {
+            fetchedCart = cart;
             return cart.getProducts();
         })
         .then(products => {
@@ -127,16 +130,25 @@ exports.postOrder = (req, res, next) => {
             console.log(products);
         })
         .then(result => {
+            return fetchedCart.setProducts(null);
+        })
+        .then(result => {
             res.redirect('/orders');
         })
         .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-    res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'Your Orders'
-    });
+    req.user
+        .getOrders({include: ['products']})
+        .then(orders => {
+            res.render('shop/orders', {
+                path: '/orders',
+                pageTitle: 'Your Orders',
+                orders: orders
+            });
+        })
+        .catch(err => console.log(err));
 };
 
 exports.getCheckout = (req, res, next) => {
