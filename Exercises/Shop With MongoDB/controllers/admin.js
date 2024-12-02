76b3ae5856088fb6c4430c28863bdb00f8eb8ea0
Frederico Santos <fred.santos@outlook.pt>
2024-12-02 15:02:02 +0000

    184. Using the Database Connection

--- Exercises/Shop With MongoDB/controllers/admin.js
+++ Exercises/Shop With MongoDB/controllers/admin.js
@@ -38,77 +38,77 @@ exports.postAddProduct = (req, res, next) => {
 
 };
 
-exports.getEditProduct = (req, res, next) => {
-  const editMode = req.query.edit;
-  if (!editMode) {
-    return res.redirect('/');
-  }
+// exports.getEditProduct = (req, res, next) => {
+//   const editMode = req.query.edit;
+//   if (!editMode) {
+//     return res.redirect('/');
+//   }
 
-  const prodId = req.params.productId;
+//   const prodId = req.params.productId;
 
-  //Product.findByPk(prodId)
-  req.user.getProducts({where: {id: prodId}})
-    .then(products => {
-      const product = products[0];
-      if (!product) {
-        return res.redirect('/');
-      }
+//   //Product.findByPk(prodId)
+//   req.user.getProducts({where: {id: prodId}})
+//     .then(products => {
+//       const product = products[0];
+//       if (!product) {
+//         return res.redirect('/');
+//       }
 
-      res.render('admin/edit-product', {
-        pageTitle: 'Edit Product',
-        path: '/admin/edit-product',
-        editing: editMode,
-        product: product
-      });
-    })
-    .catch(err => console.log(err));
-};
+//       res.render('admin/edit-product', {
+//         pageTitle: 'Edit Product',
+//         path: '/admin/edit-product',
+//         editing: editMode,
+//         product: product
+//       });
+//     })
+//     .catch(err => console.log(err));
+// };
 
-exports.postEditProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  const updatedTitle = req.body.title;
-  const updatedPrice = req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
-  const updatedDescription = req.body.description;
+// exports.postEditProduct = (req, res, next) => {
+//   const prodId = req.body.productId;
+//   const updatedTitle = req.body.title;
+//   const updatedPrice = req.body.price;
+//   const updatedImageUrl = req.body.imageUrl;
+//   const updatedDescription = req.body.description;
 
-  Product.findByPk(prodId)
-    .then(product => {
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.imageUrl = updatedImageUrl;
-      product.description = updatedDescription;
+//   Product.findByPk(prodId)
+//     .then(product => {
+//       product.title = updatedTitle;
+//       product.price = updatedPrice;
+//       product.imageUrl = updatedImageUrl;
+//       product.description = updatedDescription;
 
-      return product.save();
-    })
-    .then(result => {
-      console.log("UPDATED PRODUCT!");
-      res.redirect('/admin/product-list');
-    })
-    .catch(err => console.log(err));
-};
+//       return product.save();
+//     })
+//     .then(result => {
+//       console.log("UPDATED PRODUCT!");
+//       res.redirect('/admin/product-list');
+//     })
+//     .catch(err => console.log(err));
+// };
 
-exports.getProducts = (req, res, next) => {
-  //Product.findAll()
-  req.user.getProducts()
-    .then((products) => {
-      res.render('admin/product-list', {
-        prods: products,
-        pageTitle: 'Admin Products',
-        path: '/admin/product-list',
-      });
-    })
-    .catch(err => console.log(err));
-}
+// exports.getProducts = (req, res, next) => {
+//   //Product.findAll()
+//   req.user.getProducts()
+//     .then((products) => {
+//       res.render('admin/product-list', {
+//         prods: products,
+//         pageTitle: 'Admin Products',
+//         path: '/admin/product-list',
+//       });
+//     })
+//     .catch(err => console.log(err));
+// }
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  Product.findByPk(prodId)
-    .then(product => {
-      return product.destroy();
-    })
-    .then(result => {
-      console.log("DESTROYED PRODUCT");
-      res.redirect('/admin/product-list')
-    })
-    .catch(err => console.log(err));
-}
+// exports.postDeleteProduct = (req, res, next) => {
+//   const prodId = req.body.productId;
+//   Product.findByPk(prodId)
+//     .then(product => {
+//       return product.destroy();
+//     })
+//     .then(result => {
+//       console.log("DESTROYED PRODUCT");
+//       res.redirect('/admin/product-list')
+//     })
+//     .catch(err => console.log(err));
+// }
--- Exercises/Shop With MongoDB/util/database.js
+++ Exercises/Shop With MongoDB/util/database.js
@@ -1,17 +1,28 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = callback => {
     MongoClient.connect(
         'mongodb+srv://fred:PuNuB223oklPHi3o@cluster0.orllb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
     )
         .then(client => {
             console.log('Connected!');
-            callback(client);
+            _db = client.db();
+            callback();
         })
         .catch(err => {
             console.log(err);
         });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+    if(_db) {
+        return _db;
+    }
+    throw 'No database found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
