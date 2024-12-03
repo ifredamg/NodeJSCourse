3bb0ba853d38c96328f0d83fdb36e1f1c50a01a8
Frederico Santos <fred.santos@outlook.pt>
2024-12-03 08:27:17 +0000

    185. Creating Products

--- Exercises/Shop With MongoDB/models/product.js
+++ Exercises/Shop With MongoDB/models/product.js
@@ -10,8 +10,8 @@ class Product {
 
     save() {
         const db = getDb();
-        db.collection('products')
-            .insterOne(this)
+        return db.collection('products')
+            .insertOne(this)
             .then(result => {
                 console.log(result);
             })
