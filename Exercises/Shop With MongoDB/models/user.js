1caf95f3aa12b8321d4a90a47a37ee38cfba14c1
Frederico Santos <fred.santos@outlook.pt>
2024-12-05 14:24:41 +0000

    195. Creating New Users

--- Exercises/Shop With MongoDB/models/user.js
+++ Exercises/Shop With MongoDB/models/user.js
@@ -1,17 +1,37 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const getDb = require('../util/database');
 
-const sequelize = require('../util/database');
-const { escapeXML } = require('ejs');
+class User {
+    constructor(username, email, id) {
+        this.name = username;
+        this.email = email;
+        this._id = id ? new mongodb.ObjectId(id) : null;
+    }
 
-const User = sequelize.define('user', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    name: Sequelize.STRING,
-    email: Sequelize.STRING
-});
+    save() {
+        const db = getDb();
+        let dbOp;
+
+        if(this._id) {
+            dbOp = db.collection('users').updateOne({ _id: this._id }, { $set: this });
+        } else {
+            dbOp = db.collection('users').insertOne(this);
+        }
+        return dbOp
+            .then(result => {
+                console.log(result);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    static findById(userId) {
+        const db = getDb();
+        return db
+            .collection('users')
+            .findOne({ _id: new mongodb.ObjectId(userId) });
+    }
+}
 
 module.exports = User;
