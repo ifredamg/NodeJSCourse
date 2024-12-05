6ce1864fbfb856b25fb23887442efe8a94024f6c
Frederico Santos <fred.santos@outlook.pt>
2024-12-05 13:24:08 +0000

    192. One Note About Updating Products

--- Exercises/Shop With MongoDB/controllers/admin.js
+++ Exercises/Shop With MongoDB/controllers/admin.js
@@ -1,7 +1,4 @@
-const mongodb = require('mongodb');
 const Product = require('../models/product');
-
-const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -61,7 +58,7 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDescription = req.body.description;
 
-  const product = new Product(updatedTitle, updatedPrice, updatedDescription, updatedImageUrl, new ObjectId(prodId));
+  const product = new Product(updatedTitle, updatedPrice, updatedDescription, updatedImageUrl, prodId);
   product
     .save()
     .then(result => {
