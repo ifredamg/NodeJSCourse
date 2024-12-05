fe2c316c47117e7ae0356f75d4fb5a74a2ddc0e7
Frederico Santos <fred.santos@outlook.pt>
2024-12-05 14:25:01 +0000

    195. Creating New Users

--- Exercises/Shop With MongoDB/app.js
+++ Exercises/Shop With MongoDB/app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findById("12h1j2khek12db1cjhbcjc")
+    User.findById("12h1j2khek12db1cjhbcjc") //CREATE COLLECTION AND USER!
     .then(user => {
         req.user = user;
         next();
