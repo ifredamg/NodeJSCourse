ab4fa2b627723bead6a829050332234b16685dab
Frederico Santos <fred.santos@outlook.pt>
2024-12-06 13:19:56 +0000

    197. Working on Cart Items & Orders

--- Exercises/Shop With MongoDB/models/user.js
+++ Exercises/Shop With MongoDB/models/user.js
@@ -2,21 +2,22 @@ const mongodb = require("mongodb");
 const getDb = require('../util/database').getDb;
 
 class User {
-    constructor(username, email, id) {
+    constructor(username, email, cart) {
         this.name = username;
         this.email = email;
-        this._id = id ? new mongodb.ObjectId(id) : null;
+        //this._id = id ? new mongodb.ObjectId(id) : null;
+        this.cart = cart; // {items: []}
     }
 
     save() {
         const db = getDb();
         let dbOp;
 
-        if(this._id) {
-            dbOp = db.collection('users').updateOne({ _id: this._id }, { $set: this });
-        } else {
-            dbOp = db.collection('users').insertOne(this);
-        }
+        // if(this._id) {
+        //     dbOp = db.collection('users').updateOne({ _id: this._id }, { $set: this });
+        // } else {
+        dbOp = db.collection('users').insertOne(this);
+        // }
         return dbOp
             .then(result => {
                 console.log(result);
@@ -24,6 +25,22 @@ class User {
             .catch(err => {
                 console.log(err);
             });
+    }
+
+    addToCart(product) {
+        const cartProduct = this.cart.items.findIndex(cp => {
+            return cp._id === product._id;
+        });
+
+        const updatedCart = {
+            items: [{ ...product, quantity: 1 }]
+        };
+
+        const db = getDb();
+        return db.collection('users').insertOne(
+            { _id: new mongodb.ObjectId(this._id) },
+            { $set: { cart: updatedCart } }
+        );
     }
 
     static findById(userId) {
