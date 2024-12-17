14da34306086c49050f461ea0dc19ae5ec3b9c3d
Frederico Santos <fred.santos@outlook.pt>
2024-12-17 08:22:22 +0000

    214. Saving Data Through Mongoose

--- Exercises/Shop With Mongoose/app.js
+++ Exercises/Shop With Mongoose/app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+//const User = require('./models/user');
 
 const app = express();
 
@@ -19,21 +19,21 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    User.findById("6752c36a1b53b902440074ec")
-    .then(user => {
-        req.user = new User(user.name, user.email, user.cart, user._id);
-        next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//     User.findById("6752c36a1b53b902440074ec")
+//     .then(user => {
+//         req.user = new User(user.name, user.email, user.cart, user._id);
+//         next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://fred:PuNuB223oklPHi3o@cluster0.orllb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('mongodb+srv://fred:PuNuB223oklPHi3o@cluster0.orllb.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')
     .then(result => {
         app.listen(3000);
     })
