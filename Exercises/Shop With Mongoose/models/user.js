9de639d5427be46df8856ae0c859fd41d20136af
Frederico Santos <fred.santos@outlook.pt>
2025-01-02 08:36:52 +0000

    227. Clearing the Cart After Storing an Order

--- Exercises/Shop With Mongoose/models/user.js
+++ Exercises/Shop With Mongoose/models/user.js
@@ -26,7 +26,7 @@ const userSchema = new Schema({
     }
 });
 
-userSchema.methods.addToCart = function(product) {
+userSchema.methods.addToCart = function (product) {
     const cartProductIndex = this.cart.items.findIndex(cp => {
         return cp.productId.toString() === product._id.toString();
     });
@@ -52,12 +52,17 @@ userSchema.methods.addToCart = function(product) {
     return this.save();
 }
 
-userSchema.methods.removeFromCart = function(productId) {
+userSchema.methods.removeFromCart = function (productId) {
     const updatedCartItems = this.cart.items.filter(item => {
         return item.productId.toString() !== productId.toString();
     });
 
     this.cart.items = updatedCartItems;
+    return this.save();
+}
+
+userSchema.methods.clearCart = function () {
+    this.cart = { items: [] };
     return this.save();
 }
 
