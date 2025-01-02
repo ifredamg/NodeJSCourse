c4556d39ffa2cde5bdd07bc44246a1653b3290e9
Frederico Santos <fred.santos@outlook.pt>
2025-01-02 08:29:37 +0000

    225. Creating & Getting Orders

--- Exercises/Shop With Mongoose/controllers/shop.js
+++ Exercises/Shop With Mongoose/controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Order = require('../models/order');
 //const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
@@ -80,7 +81,22 @@ exports.postCartDeleteProduct = (req, res, next) => {
 
 exports.postOrder = (req, res, next) => {
     req.user
-        .addOrder()
+        .populate('cart.items.productId')
+        .then(user => {
+            const products = user.cart.items.map(i => {
+                return { quantity: i.quantity, product: i.productId };
+            });
+
+            const order = new Order({
+                user: {
+                    userId: req.user,
+                    name: req.user.name,
+                },
+                products: products
+            });
+
+            return order.save();
+        })
         .then(result => {
             res.redirect('/orders');
         })
