9381712c1200ba13a193d5976a272a3ec7d90d25
ifredamg <fsraen@gmail.com>
2025-01-25 20:18:28 +0000

    275. Using Nodemailer to Send an Email

--- Exercises/Shop With Mongoose/controllers/auth.js
+++ Exercises/Shop With Mongoose/controllers/auth.js
@@ -1,6 +1,14 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const User = require('../models/user');
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+  auth: {
+    api_key: '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'
+  }
+}));
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
@@ -85,6 +93,16 @@ exports.postSignup = (req, res, next) => {
         })
         .then(result => {
           res.redirect('/login');
+
+          return transporter.sendMail({
+            to: email,
+            from: 'fsraen@gmail.com',
+            subject: 'Signup succeeded!',
+            html: '<h1>You successfully signed up!</h1>'
+          });
+        })
+        .catch(err => {
+          console.log(err);
         });
     })
     .catch(err => {
