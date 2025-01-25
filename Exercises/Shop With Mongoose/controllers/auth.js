24efc2e9e387aef8e630491dd50776a62d9a60d4
ifredamg <fsraen@gmail.com>
2025-01-25 19:51:40 +0000

    269. Adding Additional Flash Messages

--- Exercises/Shop With Mongoose/controllers/auth.js
+++ Exercises/Shop With Mongoose/controllers/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
-  if(message.length > 0) {
+  if (message.length > 0) {
     message = message[0];
   } else {
     message = null;
@@ -17,9 +17,16 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render('auth/signup', {
     path: '/signup',
-    pageTitle: 'Signup'
+    pageTitle: 'Signup',
+    errorMessage: message
   });
 };
 
@@ -43,6 +50,7 @@ exports.postLogin = (req, res, next) => {
               res.redirect('/');
             });
           }
+          req.flash('error', 'Invalid email or password.');
           res.redirect('/login');
         })
         .catch(err => {
@@ -61,6 +69,7 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
+        req.flash('error', 'E-Mail exists already, please pick a different one.');
         return res.redirect('/signup');
       }
       return bcrypt
