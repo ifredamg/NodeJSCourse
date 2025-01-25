3a286a752bfab16698c8603671ed2bfea3336162
ifredamg <fsraen@gmail.com>
2025-01-25 21:41:31 +0000

    282. Creating the Reset Password Form

--- Exercises/Shop With Mongoose/controllers/auth.js
+++ Exercises/Shop With Mongoose/controllers/auth.js
@@ -135,14 +135,14 @@ exports.getReset = (req, res, next) => {
 
 exports.postReset = (req, res, next) => {
   crypto.randomBytes(32, (err, buffer) => {
-    if(err) {
+    if (err) {
       console.log(err);
       return res.redirect('/reset');
     }
     const token = buffer.toString('hex');
-    User.findOne({email: req.body.email})
+    User.findOne({ email: req.body.email })
       .then(user => {
-        if(!user) {
+        if (!user) {
           req.flash('error', 'No account with that email found.');
           return res.redirect('/reset');
         }
@@ -158,7 +158,7 @@ exports.postReset = (req, res, next) => {
           subject: 'Password reset',
           html: `
             <p>You requested a password reset</p>
-            <p>Click this <a href="http://localhost:3000/reset${token}">link</a> to set a new password.</p>
+            <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
           `
         });
       })
@@ -166,4 +166,27 @@ exports.postReset = (req, res, next) => {
         console.log(err);
       })
   })
+}
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({resetToken: token, resetTokenExpiration: { $gt: Date.now() }})
+    .then(user => {
+      let message = req.flash('error');
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      
+      res.render('auth/new-password', {
+        path: '/new-password',
+        pageTitle: 'New Password',
+        errorMessage: message,
+        userId: user._id.toString()
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
