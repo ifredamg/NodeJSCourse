34abfe21bd8af189d016374e0757741e0c8831a5
Frederico Santos <fred.santos@outlook.pt>
2025-01-26 10:33:51 +0000

    297. Checking For Field Equality

--- Exercises/Shop With Mongoose/routes/auth.js
+++ Exercises/Shop With Mongoose/routes/auth.js
@@ -28,7 +28,14 @@ router.post(
             'Please enter a password with only numbers and text and at least 5 characters.'
         )
             .isLength({ min: 5 })
-            .isAlphanumeric()
+            .isAlphanumeric(),
+        body('confirmedPassword')
+            .custom((value, {req}) => {
+                if(value !== req.body.password) {
+                    throw new Error('Password have to match!');
+                }
+                return true;
+            })
     ],
     authController.postSignup
 );
