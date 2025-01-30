64ddffbad015abf1c23084d04fa7c976e223397e
Frederico Santos <fred.santos@outlook.pt>
2025-01-30 17:27:43 +0000

    328. Serving Images Statically

--- Exercises/Shop With Mongoose/app.js
+++ Exercises/Shop With Mongoose/app.js
@@ -28,7 +28,8 @@ const fileStorage = multer.diskStorage({
     cb(null, 'images');
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname);
+    let filename = new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname;
+    cb(null, filename);
   }
 });
 
@@ -56,6 +57,7 @@ app.use(
   multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
 );
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(
   session({
     secret: 'my secret',
