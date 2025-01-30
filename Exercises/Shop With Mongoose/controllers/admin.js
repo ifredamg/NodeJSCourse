e222776865b1cc9b35a18386e44a7d496af0e81c
Frederico Santos <fred.santos@outlook.pt>
2025-01-30 13:22:08 +0000

    313. Using the Express.js Error Handling Middleware

--- Exercises/Shop With Mongoose/controllers/admin.js
+++ Exercises/Shop With Mongoose/controllers/admin.js
@@ -52,8 +52,11 @@ exports.postAddProduct = (req, res, next) => {
       res.redirect('/admin/products');
     })
     .catch(err => {
-      console.log(err);
-      res.redirect('/500');
+      // console.log(err);
+      // res.redirect('/500');
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
