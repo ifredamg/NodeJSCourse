8cb7c2c234f323219ed6a78e0fc453cb4fb0a8be
Frederico Santos <fred.santos@outlook.pt>
2025-01-30 15:07:15 +0000

    322. Handling Multipart Form Data

--- Exercises/Shop With Mongoose/routes/admin.js
+++ Exercises/Shop With Mongoose/routes/admin.js
@@ -21,7 +21,7 @@ router.post('/add-product',
             .isString()
             .isLength({ min: 3 })
             .trim(),
-        body('imageUrl').isURL(),
+        //body('imageUrl').isURL(),
         body('price').isFloat(),
         body('description')
             .isLength({ min: 5, max: 400 })
