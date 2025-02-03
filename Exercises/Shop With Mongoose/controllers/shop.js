f302ec62c5db663b11cc00ac7bb874a9423e48c0
Frederico Santos <fred.santos@outlook.pt>
2025-02-03 13:29:10 +0000

    333. Using PDFKit for .pdf Generation

--- Exercises/Shop With Mongoose/controllers/shop.js
+++ Exercises/Shop With Mongoose/controllers/shop.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const PDFDocument = require('pdfkit');
+
 const Product = require('../models/product');
 const Order = require('../models/order');
 
@@ -161,6 +163,17 @@ exports.getInvoice = (req, res, next) => {
       const invoiceName = 'invoice-' + orderId + '.pdf';
       const invoicePath = path.join('data', 'invoices', invoiceName)
 
+      const pdfDoc = new PDFDocument();
+
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"'); //attachment
+
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
+
+      pdfDoc.text('Hello world!');
+
+      pdfDoc.end();
       // fs.readFile(invoicePath, (err, data) => {
       //   if (err) {
       //     return next(err);
@@ -172,8 +185,7 @@ exports.getInvoice = (req, res, next) => {
       // });
 
       const file = fs.createReadStream(invoicePath);
-      res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"'); //attachment
+      
       file.pipe(res);
 
     })
