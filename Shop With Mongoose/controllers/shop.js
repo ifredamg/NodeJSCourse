bf5c95bd959563fd0983c3a1e2b560ac2e598d3f
Frederico Santos <fred.santos@outlook.pt>
2025-07-11 11:03:39 +0100

    357. Adding a Checkout Page

--- Shop With Mongoose/controllers/shop.js
+++ Shop With Mongoose/controllers/shop.js
@@ -9,13 +9,22 @@ const Order = require('../models/order');
 const ITEMS_PER_PAGE = 2;
 
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1; //add + to convert string to number
+  let totalItems;
+  
   Product.find()
     .then(products => {
       console.log(products);
       res.render('shop/product-list', {
         prods: products,
         pageTitle: 'All Products',
-        path: '/products'
+        path: '/products',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
