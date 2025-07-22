8e48f855ac8dab98292bbd2654c4957ca81d9269
Frederico Santos <fred.santos@outlook.pt>
2025-07-22 13:51:10 +0100

    375. Fetching Lists of Posts

--- REST API/controllers/feed.js
+++ REST API/controllers/feed.js
@@ -1,5 +1,18 @@
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({posts: [{title: 'First post', content: 'This is the first post!'}]});
+    res.status(200).json(
+        {
+            posts: [
+                {
+                    _id: '1',
+                    title: 'First post',
+                    content: 'This is the first post!',
+                    imageUrl: 'images/duck.png',
+                    creator: {
+                        name: 'Max Schwarz'
+                    },
+                    createdAt: new Date()
+                }]
+        });
 }
 
 exports.createPost = (req, res, next) => {
@@ -9,6 +22,6 @@ exports.createPost = (req, res, next) => {
     // Create post in db
     res.status(201).json({
         message: 'Post created successfully!',
-        post: {id: new Date().toISOString(), title: title, content: content}
+        post: { id: new Date().toISOString(), title: title, content: content }
     });
 }
